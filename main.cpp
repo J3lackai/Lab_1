33c60c4e314df484914e657000fed6230633a7f8
J3lackai <am19012004@gmail.com>
2026-03-02 00:16:35 +0700

    Реализована функция для обхода папок и файлов в нужной директории
    - Функция для обхода print_all
    - Вспомогательные функции print_attribs, fileSize
    - Добавлены нужные импорты
    - В функции main происходит запуск print_all
    - В gitignore добавлена папка build

--- main.cpp
+++ main.cpp
@@ -1,8 +1,57 @@
+#include <iostream>
 #include <QCoreApplication>
+#include <QDirIterator>
+#include <QFileInfo>
+using namespace std;
+void print_attribs(QFileInfo& info, QTextStream& stream) {
+    if (info.isReadable())
+        stream << "R";
+    if (info.isWritable())
+        stream << "W";
+    if (info.isHidden())
+        stream << "H";
+    if (info.isExecutable())
+        stream << "E";
+}
 
-int main(int argc, char *argv[])
+QString fileSize(qint64 nSize) {
+    qint64 i = 0;
+    for (; nSize > 1023; nSize /= 1024, ++i) { }
+    return QString().setNum(nSize) + "BKMGT"[i];
+}
+void print_all(QString path, QTextStream& stream) {
+    QDirIterator itDirs(path, QDir::Dirs);
+    while (itDirs.hasNext()) {
+        itDirs.next();
+
+        if (itDirs.fileName() == ".")
+            continue;
+
+        QFileInfo info = itDirs.fileInfo();
+        stream << "D | " << info.fileName() << " | " << fileSize(info.size()) << " | "
+               << info.lastModified().toString() << " | ";
+        print_attribs(info, stream);
+        stream << "\n";
+    }
+
+    QDirIterator itFiles(path, QDir::Files);
+    while (itFiles.hasNext()) {
+        itFiles.next();
+
+        QFileInfo info = itFiles.fileInfo();
+        stream << "F | " << info.fileName() << " | " << fileSize(info.size()) << " | "
+               << info.lastModified().toString() << " | ";
+        print_attribs(info, stream);
+        stream << "\n";
+    }
+}
+
+
+int main()
 {
-    //QCoreApplication a(argc, argv);
-
-    //return a.exec();
+    QString path("C:\\qt15.0.1\\projects\\Lab_1");
+    QTextStream stream(stdout);
+    print_all(path,stream);
+    cout << "Complete!";
+    return 0;
 }
