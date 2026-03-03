520a6d8cf34bcfca4763c7c3632de1cc1e375655
J3lackai <am19012004@gmail.com>
2026-03-04 00:06:22 +0700

    Реализованы методы processFile, deriveKey

--- Encryptor.cpp
+++ Encryptor.cpp
@@ -1,6 +1,7 @@
 #include <Encryptor.h>
 #include <QDir>
 #include <QDirIterator>
+#include <QCryptographicHash>
 Encryptor& Encryptor::getInstance() {
     //Объект создаём один раз при первом вызове функции, затем передаём его по ссылке
     static Encryptor instance;
@@ -38,4 +39,37 @@ bool Encryptor::encryptDirectory(const QString& path, const QString& password) {
 
 bool Encryptor::decryptDirectory(const QString& path, const QString& password) {
     return traverseDirectory(path, password, false);
+}
+bool Encryptor::processFile(const QString& filePath,
+                            const QString& password, bool encrypt) {
+    QFile file(filePath);
+    if (!file.open(QIODevice::ReadOnly)) {
+        qWarning() << "Cannot open file for reading:" << filePath;
+        return false;
+    }
+    file.close();
+
+    QByteArray key = deriveKey(password);
+    QString outputPath = filePath + (encrypt ? ".enc" : "");// формируем путь к файлу
+
+    if (encrypt) {
+        encryptFile(filePath, outputPath, key);
+        QFile::remove(filePath); // Удаляем оригинал после шифрования
+    } else {
+        // Для дешифрования убираем расширение .enc
+        if (filePath.endsWith(".enc")) {
+            outputPath = filePath.left(filePath.length() - 4); // Убираем .enc
+            decryptFile(filePath, outputPath, key);
+            QFile::remove(filePath); // Удаляем зашифрованный файл
+        } else {
+            qDebug() << "Skipping non-encrypted file:" << filePath;
+            return false;
+        }
+    }
+    return true;
+}
+
+QByteArray Encryptor::deriveKey(const QString& password) {
+    return QCryptographicHash::hash(password.toUtf8(), QCryptographicHash::Sha256);
+    //Из пароля генерируем SHA-256 хеш, наш ключ
 }
