f38d48145002bd5e641e837332c1ba7cb55701cc
J3lackai <am19012004@gmail.com>
2026-03-04 23:51:52 +0700

    Заменён ввод cin, теперь пути с пробелами правильно обрабатываются

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <QCoreApplication>
 #include <QDirIterator>
 #include <QFileInfo>
@@ -12,21 +11,22 @@ using namespace std;
 
 int main()
 {
-    string raw_path, raw_pswrd, mode;
+    QTextStream in(stdin);
+
     qInfo() << "Input path: ";
-    cin >> raw_path;
+    QString path = in.readLine();
+
     qInfo() << "Input password: ";
-    cin >> raw_pswrd;
+    QString pswrd = in.readLine();
     qInfo() << "\nBefore decrypting, be sure to save a copy of the encrypted files,\n"
                "because if the password was entered incorrectly, the data will be permanently lost!!!\n";
-
+    string mode;
     while(mode != "e" && mode != "d")
     {
         qInfo() << "Input 'e' for encode, 'd' for decode: ";
-        cin >> mode;
+        QString Qmode = in.readLine();
+        mode = Qmode.toStdString();
     }
-    QString path = QString::fromStdString(raw_path);//Преобразуем путь: String -> QString
-    QString pswrd = QString::fromStdString(raw_pswrd);//Преобразуем пароль: String -> QString
     Encryptor& enc = Encryptor::getInstance();
     QString res;
     if (mode == "e")
