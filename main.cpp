c94f722e2e5554a60592acf8d90e38050d04b804
J3lackai <am19012004@gmail.com>
2026-03-04 20:44:41 +0700

    Создаётся объект шифратора, импортирован QDebug для вывода в консоль

--- main.cpp
+++ main.cpp
@@ -3,18 +3,24 @@
 #include <QDirIterator>
 #include <QFileInfo>
 #include <string>
-#include <Encryptor.h>
+#include <QDebug>
+#include <QTextStream>
+#include <Encryptor.cpp>
+
 using namespace std;
 
 
 int main()
 {
-    string raw_path;
-    QTextStream out(stdout);
-    out << "Input path: ";
-    out.flush();  // Принудительно выводим сообщение
-    cin >> raw_path; //Пример C:\qt15.0.1\projects\Lab_1
-    QTextStream stream(stdout);
+    string raw_path, raw_pswrd;
+    qInfo() << "Input path: ";
+    cin >> raw_path;
+    qInfo() << "Input password: ";
+    cin >> raw_pswrd;
     QString path = QString::fromStdString(raw_path);//Преобразуем путь: String -> QString
+    QString pswrd = QString::fromStdString(raw_pswrd);//Преобразуем пароль: String -> QString
+    Encryptor& enc = Encryptor::getInstance();
+    QString res = enc.encryptDirectory(path, pswrd) ? "\nComplete encrypt!" : "\nError encrypt!";
+    qInfo() << res;
     return 0;
 }
